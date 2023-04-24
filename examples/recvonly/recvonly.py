bac314f168c00d9acb76535ddcc09cf347f63f3f
voluntas <nakai@shiguredo.jp>
2023-04-24 23:23:27 +0900

    リファクタリング

--- examples/recvonly/recvonly.py
+++ examples/recvonly/recvonly.py
@@ -3,81 +3,85 @@ import time
 
 import cv2
 import sounddevice
+
 from sora_sdk import Sora, SoraAudioSink, SoraVideoSink
 
-use_hardware_encoder = False
-output_frequency = 16000
-output_channels = 1
 
-sora = Sora(use_hardware_encoder)
-connection = sora.create_connection(
-    signaling_url="signaling_url",
-    role="recvonly",
-    channel_id="channel_id",
-    client_id="recvonly",
-    metadata={'access_token': 'access_token'}
-)
+class Recvonly:
+    def __init__(self, signaling_url, channel_id, client_id,
+                 access_token, use_hardware_encoder=False,
+                 output_frequency=16000, output_channels=1):
+        self.use_hardware_encoder = use_hardware_encoder
+        self.output_frequency = output_frequency
+        self.output_channels = output_channels
 
-disconnected = False
+        self.sora = Sora(self.use_hardware_encoder)
+        self.connection = self.sora.create_connection(
+            signaling_url=signaling_url,
+            role="recvonly",
+            channel_id=channel_id,
+            client_id=client_id,
+            metadata={'access_token': access_token}
+        )
+
+        self.disconnected = False
+        self.connection.on_disconnect = self.on_disconnect
+
+        self.audio_sink = None
+        self.video_sink = None
+        self.q_out = queue.Queue()
+
+        self.connection.on_track = self.on_track
+
+    def on_disconnect(self, ec, message):
+        self.disconnected = True
+        print(message)
+
+    def on_frame(self, frame):
+        self.q_out.put(frame)
+
+    def on_track(self, track):
+        if track.kind == "audio":
+            self.audio_sink = SoraAudioSink(
+                track, self.output_frequency, self.output_channels)
+        if track.kind == "video":
+            self.video_sink = SoraVideoSink(track)
+            self.video_sink.on_frame = self.on_frame
+
+    def callback(self, outdata, frames, time, status):
+        if self.audio_sink is not None:
+            success, data = self.audio_sink.read(frames)
+            if success:
+                if data.shape[0] != frames:
+                    print("AUDIO_DATA_NOT_ENOUGH", data.shape, frames)
+                outdata[:] = data
+            else:
+                print("CAN_NOT_GET_AUDIO_DATA")
+
+    def run(self):
+        with sounddevice.OutputStream(channels=self.output_channels, callback=self.callback,
+                                      samplerate=self.output_frequency, dtype='int16'):
+            self.connection.connect()
+
+            while True:
+                frame = self.q_out.get()
+                cv2.imshow('frame', frame.data())
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+            self.connection.disconnect()
+
+            while not self.disconnected:
+                time.sleep(0.01)
+
+        cv2.destroyAllWindows()
 
 
-def on_disconnect(ec, message):
-    global disconnected
-    disconnected = True
-    print(message)
+if __name__ == '__main__':
+    signaling_url = "signaling_url"
+    channel_id = "channel_id"
+    client_id = "recvonly"
+    access_token = "access_token"
 
-
-connection.on_disconnect = on_disconnect
-
-audio_sink = None
-video_sink = None
-
-q_out = queue.Queue()
-
-
-def on_frame(frame):
-    global q_out
-    q_out.put(frame)
-
-
-def on_track(track):
-    global audio_sink, video_sink
-    if track.kind == "audio":
-        # ここで指定した output_frequency と output_channels に resampling 、 remix されて出力する
-        audio_sink = SoraAudioSink(track, output_frequency, output_channels)
-    if track.kind == "video":
-        video_sink = SoraVideoSink(track)
-        video_sink.on_frame = on_frame
-
-
-def callback(outdata, frames, time, status):
-    global audio_sink
-    if audio_sink is not None:
-        success, data = audio_sink.read(frames)
-        if success:
-            if data.shape[0] != frames:
-                print("AUDIO_DATA_NOT_ENOUGH", data.shape, frames)
-            outdata[:] = data
-        else:
-            print("CAN_NOT_GET_AUDIO_DATA")
-
-
-connection.on_track = on_track
-
-with sounddevice.OutputStream(channels=output_channels, callback=callback,
-                              samplerate=output_frequency, dtype='int16'):
-    connection.connect()
-
-    while True:
-        # frame を queue から取り出したとき何故か一度変数にしてあげないとクラッシュする
-        frame = q_out.get()
-        cv2.imshow('frame', frame.data())
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    connection.disconnect()
-
-    while not disconnected:
-        time.sleep(0.01)
-
-cv2.destroyAllWindows()
+    recvonly = Recvonly(signaling_url, channel_id, client_id, access_token)
+    recvonly.run()
