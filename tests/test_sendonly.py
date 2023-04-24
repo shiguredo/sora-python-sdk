6158ac859ef7cdd4b0a645d415321d15266d8bdf
voluntas <nakai@shiguredo.jp>
2023-04-24 22:13:03 +0900

    コメント

--- tests/test_sendonly.py
+++ tests/test_sendonly.py
@@ -4,6 +4,7 @@ import signal
 import cv2
 import sounddevice
 
+# Sora が見たらないといわれてる
 from sora_sdk import Sora
 
 use_hardware_encoder = False
@@ -12,6 +13,15 @@ samplerate = 16000
 
 
 def test_sendonly():
+    def handler(signum, frame):
+        global running
+        running = False
+
+    def callback(indata, frames, time, status):
+        global audio_source
+        # audio_sourceが見当たらないといわれる
+        audio_source.on_data(indata)
+
     # ここで ffmpeg で仮想カメラを作成する
 
     sora = Sora(use_hardware_encoder)
@@ -37,6 +47,8 @@ def test_sendonly():
     signal.signal(signal.SIGINT, handler)
 
     with sounddevice.InputStream(samplerate=samplerate, channels=channels, dtype='int16', callback=callback):
+        # ここがそもそも非同期なのがわかりやすくあってほしいかも
+        # await とか付いてると嬉しそう
         connection.connect()
 
         while running:
@@ -45,15 +57,8 @@ def test_sendonly():
                 continue
             video_source.on_captured(frame)
 
+            # キーボードで抜ける仕組み
+            # expect: KeyboardInterrupt
+
     connection.disconnect()
     video_capture.release()
-
-
-def handler(signum, frame):
-    global running
-    running = False
-
-
-def callback(indata, frames, time, status):
-    global audio_source
-    audio_source.on_data(indata)
