e9a4dd2a13524ddbd57b286bf5ac38f474a0697b
voluntas <nakai@shiguredo.jp>
2023-04-24 21:21:03 +0900

    tests/ を追加

---
+++ tests/test_sendonly.py
@@ -0,0 +1,58 @@
+import os
+import signal
+
+import cv2
+import sounddevice
+
+from sora_sdk import Sora
+
+use_hardware_encoder = False
+channels = 1
+samplerate = 16000
+
+
+def test_sendonly():
+    # ここで ffmpeg で仮想カメラを作成する
+
+    sora = Sora(use_hardware_encoder)
+    audio_source = sora.create_audio_source(channels, samplerate)
+    video_source = sora.create_video_source()
+
+    signaling_url = os.environ.get('SIGNALING_URL')
+    access_token = os.environ.get('ACCESS_TOKEN')
+
+    connection = sora.create_connection(
+        signaling_url=signaling_url,
+        role="sendonly",
+        channel_id="channel_id",
+        client_id="sendonly",
+        metadata={'access_token': access_token},
+        audio_source=audio_source,
+        video_source=video_source
+    )
+    video_capture = cv2.VideoCapture(0)
+    running = True
+
+    signal.signal(signal.SIGINT, handler)
+
+    with sounddevice.InputStream(samplerate=samplerate, channels=channels, dtype='int16', callback=callback):
+        connection.connect()
+
+        while running:
+            success, frame = video_capture.read()
+            if not success:
+                continue
+            video_source.on_captured(frame)
+
+    connection.disconnect()
+    video_capture.release()
+
+
+def handler(signum, frame):
+    global running
+    running = False
+
+
+def callback(indata, frames, time, status):
+    global audio_source
+    audio_source.on_data(indata)
