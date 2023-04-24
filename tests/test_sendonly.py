7a52ebacb23bb9250349d78ab8ae1c904f217802
voluntas <nakai@shiguredo.jp>
2023-04-24 23:07:06 +0900

    リファクタリングかつテスト追加

--- tests/test_sendonly.py
+++ tests/test_sendonly.py
@@ -1,64 +1,68 @@
 import os
 import signal
+import threading
+import time
 
 import cv2
+import pytest
 import sounddevice
 
-# Sora が見たらないといわれてる
 from sora_sdk import Sora
 
-use_hardware_encoder = False
-channels = 1
-samplerate = 16000
+
+class Sendonly:
+    def __init__(self):
+        self.use_hardware_encoder = False
+        self.channels = 1
+        self.samplerate = 16000
+        self.running = True
+
+    def handler(self, signum, frame):
+        self.running = False
+
+    def callback(self, indata, frames, time, status):
+        self.audio_source.on_data(indata)
+
+    def stop(self):
+        self.running = False
+
+    def run(self):
+        sora = Sora(self.use_hardware_encoder)
+        self.audio_source = sora.create_audio_source(
+            self.channels, self.samplerate)
+        video_source = sora.create_video_source()
+        connection = sora.create_connection(
+            signaling_url=os.environ.get("TEST_SIGNALING_URL"),
+            role="sendonly",
+            channel_id=os.environ.get("TEST_CHANNEL_ID_PREFIX"),
+            client_id="sendonly",
+            metadata={'access_token': os.environ.get("TEST_SECRET_KEY")},
+            audio_source=self.audio_source,
+            video_source=video_source
+        )
+
+        video_capture = cv2.VideoCapture(0)
+
+        signal.signal(signal.SIGINT, self.handler)
+
+        with sounddevice.InputStream(samplerate=self.samplerate, channels=self.channels, dtype='int16', callback=self.callback):
+            connection.connect()
+
+            while self.running:
+                success, frame = video_capture.read()
+                if not success:
+                    continue
+                video_source.on_captured(frame)
+
+        connection.disconnect()
+        video_capture.release()
 
 
+@pytest.mark.timeout(10)
 def test_sendonly():
-    def handler(signum, frame):
-        global running
-        running = False
-
-    def callback(indata, frames, time, status):
-        global audio_source
-        # audio_sourceが見当たらないといわれる
-        audio_source.on_data(indata)
-
-    # ここで ffmpeg で仮想カメラを作成する
-
-    sora = Sora(use_hardware_encoder)
-    audio_source = sora.create_audio_source(channels, samplerate)
-    video_source = sora.create_video_source()
-
-    signaling_url = os.environ.get('TEST_SIGNALING_URL')
-    access_token = os.environ.get('TEST_ACCESS_TOKEN')
-    channel_id_prefix = os.environ.get('TEST_CHANNEL_ID_PREFIX')
-
-    connection = sora.create_connection(
-        signaling_url=signaling_url,
-        role="sendonly",
-        channel_id=f'{channel_id_prefix}_sendonly',
-        client_id="sendonly",
-        metadata={'access_token': access_token},
-        audio_source=audio_source,
-        video_source=video_source
-    )
-    video_capture = cv2.VideoCapture(0)
-    running = True
-
-    signal.signal(signal.SIGINT, handler)
-
-    with sounddevice.InputStream(samplerate=samplerate, channels=channels, dtype='int16', callback=callback):
-        # ここがそもそも非同期なのがわかりやすくあってほしいかも
-        # await とか付いてると嬉しそう
-        connection.connect()
-
-        while running:
-            success, frame = video_capture.read()
-            if not success:
-                continue
-            video_source.on_captured(frame)
-
-            # キーボードで抜ける仕組み
-            # expect: KeyboardInterrupt
-
-    connection.disconnect()
-    video_capture.release()
+    sendonly = Sendonly()
+    sendonly_thread = threading.Thread(target=sendonly.run)
+    sendonly_thread.start()
+    time.sleep(5)
+    sendonly.stop()
+    sendonly_thread.join()
