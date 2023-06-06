3ac2fa23d9bac25db652f0f60974ee1305a72949
Takeru Ohta <phjgt308@gmail.com>
2023-06-06 16:29:58 +0900

    Add messaging-sendrecv.py

--- test/messaging-sendrecv.py
+++ test/messaging-sendrecv.py
@@ -6,22 +6,24 @@ import time
 from sora_sdk import Sora
 
 
-class MessagingRecvonly:
-    def __init__(self, signaling_url, channel_id, client_id, labels, metadata):
+class MessagingSendrecv:
+    def __init__(self, signaling_url, channel_id, client_id, data_channels, metadata):
         self.sora = Sora()
         self.connection = self.sora.create_connection(
             signaling_url=signaling_url,
-            role="recvonly",
+            role="sendrecv",
             channel_id=channel_id,
             client_id=client_id,
             metadata=metadata,
-            data_channels=[{"label": label, "direction": "recvonly"}
-                           for label in labels],
+            data_channels=data_channels,
             data_channel_signaling=True,
         )
 
+        self.data_channels = data_channels
         self.disconnected = False
         self.shutdown = False
+        self.sendable_data_channels = set()
+        self.connection.on_data_channel = self.on_data_channel
         self.connection.on_message = self.on_message
         self.connection.on_disconnect = self.on_disconnect
 
@@ -30,6 +32,15 @@ class MessagingRecvonly:
 
     def on_message(self, label, data):
         print(f"メッセージを受信しました: label={label}, data={data}")
+
+    def on_data_channel(self, label):
+        for data_channel in self.data_channels:
+            if data_channel["label"] != label:
+                continue
+
+            if data_channel["direction"] == "sendrecv" or data_channel["direction"] == "sendonly":
+                self.sendable_data_channels.add(label)
+                break
 
     def exit_gracefully(self, signal_number, frame):
         print("\nCtrl+Cが押されました。終了します")
@@ -42,8 +53,16 @@ class MessagingRecvonly:
 
         self.connection.connect()
 
+        i = 0
         while not self.shutdown:
+            if i % 100 == 0:
+                for label in self.sendable_data_channels:
+                    data = f"No. {i / 100}".encode("utf-8")
+                    print(f"メッセージを送信します: label={label}, data={data}")
+                    self.connection.send_data_channel(label, data)
+
             time.sleep(0.01)
+            i += 1
 
         self.connection.disconnect()
 
@@ -58,8 +77,8 @@ if __name__ == '__main__':
     # 必須引数
     parser.add_argument("--signaling-url", required=True, help="シグナリング URL")
     parser.add_argument("--channel-id", required=True, help="チャネルID")
-    parser.add_argument("--labels", required=True, nargs='+',
-                        help="受信するデータチャネルのラベル名（複数指定可能）")
+    parser.add_argument("--data-channels", required=True,
+                        help="使用するデータチャネルを JSON で指定する (例: '[{\"label\": \"#spam\", \"direction\": \"sendrecv\"}]')")
 
     # オプション引数
     parser.add_argument("--client_id",default='',  help="クライアントID")
@@ -70,9 +89,9 @@ if __name__ == '__main__':
     if args.metadata:
         metadata = json.loads(args.metadata)
 
-    messaging_recvonly = MessagingRecvonly(args.signaling_url,
+    messaging_sendrecv = MessagingSendrecv(args.signaling_url,
                                            args.channel_id,
                                            args.client_id,
-                                           args.labels,
+                                           json.loads(args.data_channels),
                                            metadata)
-    messaging_recvonly.run()
+    messaging_sendrecv.run()
