958dd340fca8b1d2a4407e6cbae3b5c4fc3d4639
Takeru Ohta <phjgt308@gmail.com>
2023-06-09 14:53:38 +0900

    sendonly.py と recvonly.py をメッセージングのサンプルと同様に更新

--- test/recvonly.py
+++ test/recvonly.py
@@ -1,3 +1,4 @@
+import argparse
 import queue
 import signal
 import time
@@ -25,7 +26,7 @@ class Recvonly:
             metadata=metadata
         )
 
-        self.disconnected = False
+        self.shutdown = False
         self.connection.on_disconnect = self.on_disconnect
 
         self.audio_sink = None
@@ -35,8 +36,8 @@ class Recvonly:
         self.connection.on_track = self.on_track
 
     def on_disconnect(self, ec, message):
-        self.disconnected = True
-        print(message)
+        print(f"Sora から切断されました: message='{message}'")
+        self.shutdown = True
 
     def on_frame(self, frame):
         self.q_out.put(frame)
@@ -61,9 +62,7 @@ class Recvonly:
 
     def exit_gracefully(self, signal_number, frame):
         print("\nCtrl+Cが押されました。終了します")
-        self.connection.disconnect()
-        cv2.destroyAllWindows()
-        exit(0)
+        self.shutdown = True
 
     def run(self):
         # シグナルを登録し、プログラムが終了するときに正常に処理が行われるようにする
@@ -73,34 +72,41 @@ class Recvonly:
                                       samplerate=self.output_frequency, dtype='int16'):
             self.connection.connect()
 
-            while True:
-                # Windows 環境の場合 timeout を入れておかないと Queue.get() で
-                # ブロックしたときに脱出方法がなくなる。
-                try:
-                    frame = self.q_out.get(timeout=1)
-                except queue.Empty:
-                    continue
-                cv2.imshow('frame', frame.data())
-                # これは削除してよさそう
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
+            try:
+                while not self.shutdown:
+                    # Windows 環境の場合 timeout を入れておかないと Queue.get() で
+                    # ブロックしたときに脱出方法がなくなる。
+                    try:
+                        frame = self.q_out.get(timeout=1)
+                    except queue.Empty:
+                        continue
+                    cv2.imshow('frame', frame.data())
+                    # これは削除してよさそう
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+            finally:
+                self.connection.disconnect()
 
-            self.connection.disconnect()
-
-            # 切断が完了するまで待機
-            while not self.disconnected:
-                time.sleep(0.01)
-
-        # すべてのウィンドウを破棄
-        cv2.destroyAllWindows()
+                # すべてのウィンドウを破棄
+                cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    signaling_url = "signaling_url"
-    channel_id = "channel_id"
-    client_id = "recvonly"
-    access_token = "access_token"
-    metadata = {'access_token': access_token}
+    parser = argparse.ArgumentParser()
 
-    recvonly = Recvonly(signaling_url, channel_id, client_id, metadata)
+    # 必須引数
+    parser.add_argument("--signaling-url", required=True, help="シグナリング URL")
+    parser.add_argument("--channel-id", required=True, help="チャネルID")
+
+    # オプション引数
+    parser.add_argument("--client_id", default='',  help="クライアントID")
+    parser.add_argument("--metadata", help="メタデータ JSON")
+    args = parser.parse_args()
+
+    metadata = None
+    if args.metadata:
+        metadata = json.loads(args.metadata)
+
+    recvonly = Recvonly(args.signaling_url, args.channel_id,
+                        args.client_id, args.metadata)
     recvonly.run()
