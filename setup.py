d5cb9955dcf44c16005158f7d5d65da5b7d25a2a
melpon <shigemasa7watanabe+github@gmail.com>
2023-06-17 11:07:24 +0900

    auditwheel の適用をやめてみる

--- setup.py
+++ setup.py
@@ -13,13 +13,24 @@ from run import get_build_platform, PlatformTarget, cd  # noqa: E402
 
 
 def run_setup(build_platform, target_platform):
+    plat = None
+    if target_platform.os == 'jetson':
+        plat = 'manylinux_2_31_aarch64'
+    elif target_platform.os == 'ubuntu' and target_platform.arch == 'x86_64':
+        if target_platform.osver == '20.04':
+            plat = 'manylinux_2_31_x86_64'
+        if target_platform.osver == '22.04':
+            plat = 'manylinux_2_35_x86_64'
+
     class bdist_wheel(_bdist_wheel):
-        def get_tag(self):
+        def finalize_options(self):
+            _bdist_wheel.finalize_options(self)
             self.root_is_pure = False
-            _, _, plat = super().get_tag()
-            self.root_is_pure = True
+
+        def get_tag(self):
+            _, _, plat2 = super().get_tag()
             impl = 'cp' + sysconfig.get_config_var("py_version_nodot")
-            return impl, impl, plat
+            return impl, impl, plat if plat is not None else plat2
 
     setup(
         url="https://github.com/shiguredo/sora-python-sdk",
