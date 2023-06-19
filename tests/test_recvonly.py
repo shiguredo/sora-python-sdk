f68ada1562e4e1400ff8a0160e64d5101ec5c415
voluntas <nakai@shiguredo.jp>
2023-06-19 19:59:05 +0900

    更新

--- tests/test_recvonly.py
+++ tests/test_recvonly.py
@@ -6,17 +6,17 @@ from sora_sdk import Sora
 
 def on_disconnect(error_code, message_abc: str):
     print(f'on_disconnect: error_code: {error_code}, message: {message_abc}')
-    pass
 
 
 # @pytest.mark.timeout(10)
 def test_sendonly():
     sora = Sora()
+
     conn = sora.create_connection(
         signaling_url=os.environ.get("TEST_SIGNALING_URL"),
         role="recvonly",
         channel_id=os.environ.get("TEST_CHANNEL_ID_PREFIX") + "sora-python-sdk-test",
-        metadata={"access_token": os.environ.get("TEST_SECREt_KEY")}
+        metadata={"access_token": os.environ.get("TEST_SECRET_KEY")}
     )
 
     conn.on_disconnect = on_disconnect
