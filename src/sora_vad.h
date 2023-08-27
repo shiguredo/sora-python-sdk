433ae5281df0bd7227519a46c905dab79248d227
tnoho <teaotya@gmail.com>
2023-08-27 13:17:31 +0900

    VAD にコメントを追加する

--- src/sora_vad.h
+++ src/sora_vad.h
@@ -15,10 +15,26 @@
 
 namespace nb = nanobind;
 
+/**
+ * SoraAudioFrame の音声データに音声である率を返す VAD です。
+ * 
+ * 受信した音声データに対して何らかの処理を Python で行う場合、
+ * 全体の負荷軽減を考えるのであれば音声データから音声であると推測されるデータのみに、
+ * 処理を行うようにした方が全体の負荷を下げることができます。
+ * libwebrtc には優秀な VAD が含まれているため、これを活用したユーティリティクラスとして用意しました。
+ */
 class SoraVAD {
  public:
   SoraVAD();
 
+  /**
+   * SoraAudioFrame 内の音声データが音声である確率を返します。
+   * 
+   * libwebrtc 内部では 0.95 より大きい場合に音声とみなしています。
+   * 
+   * @param frame 音声である確率を求める SoraAudioFrame
+   * @return 0 - 1 で表される音声である確率
+   */
   float Analyze(std::shared_ptr<SoraAudioFrame> frame);
 
  private:
