786805a63a4508fd3029e14348593d3ef35f31e2
voluntas <nakai@shiguredo.jp>
2024-01-16 14:40:51 +0900

    null チェック

--- src/sora.cpp
+++ src/sora.cpp
@@ -269,7 +269,9 @@ Sora::ConvertForwardingFilter(const nb::handle value) {
 
   try {
     auto object = forwarding_filter_value.as_object();
-    filter.action = std::string(object["action"].as_string());
+    if (!object["action"].is_null()) {
+      filter.action = std::string(object["action"].as_string());
+    }
     for (auto or_rule : object["rules"].as_array()) {
       std::vector<sora::SoraSignalingConfig::ForwardingFilter::Rule> rules;
       for (auto and_rule_value : or_rule.as_array()) {
