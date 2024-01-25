d0237e25793a30bec5cc62c50cf13c64bcdf6c04
voluntas <nakai@shiguredo.jp>
2024-01-25 14:32:12 +0900

    メッセージを送信するクライアントを追加

--- tests/test_messaging.py
+++ tests/test_messaging.py
@@ -12,7 +12,7 @@ def on_message(label, data):
     print("on_message", label, data)
 
 
-def test_sendonly():
+def send_message():
     sora = sora_sdk.Sora()
     connection = sora.create_connection(
         signaling_urls=[os.environ.get("TEST_SIGNALING_URL")],
@@ -31,8 +31,32 @@ def test_sendonly():
 
     connection.connect()
 
-    try:
-        while True:
-            time.sleep(1)
-    finally:
-        connection.disconnect()
+    time.sleep(3)
+
+    connection.send_data_channel("#spam", "ham")
+
+    connection.disconnect()
+
+
+def test_messaging_direction_recvonly():
+    sora = sora_sdk.Sora()
+    connection = sora.create_connection(
+        signaling_urls=[os.environ.get("TEST_SIGNALING_URL")],
+        role="recvonly",
+        channel_id=os.environ.get("TEST_CHANNEL_ID_PREFIX") + "sora-python-sdk-test",
+        data_channel_signaling=True,
+        data_channels=[{"label": "#spam", "direction": "sendrecv"}],
+        audio=False,
+        video=False,
+    )
+
+    connection.on_data_channel = on_data_channel
+
+    # 次の行をコメントアウトすると SIGSEGV は発生しない
+    connection.on_message = on_message
+
+    connection.connect()
+
+    send_message()
+
+    connection.disconnect()
