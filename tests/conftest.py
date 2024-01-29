44a2e138718dac73a2109545cddf76e634d759b5
voluntas <nakai@shiguredo.jp>
2024-01-29 12:48:55 +0900

    channel_id を uuid 入れて毎回ランダムにする

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 
 import pytest
 from dotenv import load_dotenv
@@ -10,7 +11,8 @@ def setup():
     load_dotenv()
     return {
         "signaling_urls": [os.environ.get("TEST_SIGNALING_URL")],
-        "channel_id": os.environ.get("TEST_CHANNEL_ID_PREFIX") + "sora-python-sdk-test",
+        # channel_id に利用する文字列をランダムにする
+        "channel_id": f"${os.environ.get("TEST_CHANNEL_ID_PREFIX")}${uuid.uuid4().hex}",
         "secret": os.environ.get("TEST_SECRET_KEY"),
         "metadata": {"access_token": os.environ.get("TEST_SECRET_KEY")},
     }
