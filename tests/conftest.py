2b1134cfed2841fa8f6a848eb3326f0d05fb5635
voluntas <nakai@shiguredo.jp>
2024-01-31 12:09:17 +0900

    不要なコードを削除

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import os
-import uuid
 
 import pytest
 from dotenv import load_dotenv
@@ -12,8 +11,6 @@ def setup():
     return {
         "signaling_urls": [os.environ.get("TEST_SIGNALING_URL")],
         "channel_id_prefix": os.environ.get("TEST_CHANNEL_ID_PREFIX"),
-        # 乱数に使う場合にどうぞ
-        "uuid": uuid.uuid4().hex,
         "secret": os.environ.get("TEST_SECRET_KEY"),
         "metadata": {"access_token": os.environ.get("TEST_SECRET_KEY")},
     }
