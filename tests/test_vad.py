aca31750efbb228503094360524015b281d49781
voluntas <nakai@shiguredo.jp>
2024-02-19 16:23:40 +0900

    VAD の e2e テストコードを追加

---
+++ tests/test_vad.py
@@ -0,0 +1,112 @@
+import json
+import time
+from threading import Event
+
+import numpy as np
+from sora_sdk import (
+    Sora,
+    SoraAudioFrame,
+    SoraAudioStreamSink,
+    SoraConnection,
+    SoraMediaTrack,
+    SoraVAD,
+)
+
+
+class VAD:
+    _vad: SoraVAD = SoraVAD()
+
+    _sora: Sora = None
+    _connection: SoraConnection
+
+    _connection_id: str
+
+    # 接続した
+    _connected: Event = Event()
+    # 終了
+    _closed: bool = False
+
+    _audio_stream_sink: SoraAudioStreamSink
+    _audio_output_frequency: int = 24000
+    _audio_output_channels: int = 1
+
+    def __init__(self, signaling_urls: list, channel_id: str, metadata: dict):
+        self._sora = Sora()
+        self._connected = Event()
+
+        self._connection = self._sora.create_connection(
+            signaling_urls=signaling_urls,
+            role="recvonly",
+            channel_id=channel_id,
+            metadata=metadata,
+            audio=True,
+            video=False,
+        )
+
+        self._connection.on_set_offer = self._on_set_offer
+        self._connection.on_notify = self._on_notify
+        self._connection.on_disconnect = self._on_disconnect
+
+        self._connection.on_track = self._on_track
+
+    def connect(self):
+        self._connection.connect()
+
+        # _connected が set されるまで 30 秒待つ
+        assert self._connected.wait(30)
+
+        return self
+
+    def _on_set_offer(self, raw_offer):
+        offer = json.loads(raw_offer)
+        if offer["type"] == "offer":
+            self.connection_id = offer["connection_id"]
+            print(f"Offer を受信しました: connection_id={self.connection_id}")
+
+    def _on_notify(self, raw_message):
+        message = json.loads(raw_message)
+        if (
+            message["type"] == "notify"
+            and message["event_type"] == "connection.created"
+            and message["connection_id"] == self.connection_id
+        ):
+            print(f"Sora に接続しました: connection_id={self.connection_id}")
+            self._connected.set()
+
+    def _on_disconnect(self, error_code, message):
+        print(f"Sora から切断しました: error_code='{error_code}' message='{message}'")
+        self._closed = True
+        self._connected.clear()
+
+    def _on_frame(self, frame: SoraAudioFrame):
+        # frame が音声である確率を求める
+        voice_probability = self._vad.analyze(frame)
+        if voice_probability > 0.95:  # 0.95 は libwebrtc の判定値
+            print(f"Voice! voice_probability={voice_probability}")
+        else:
+            print("Not a voice!")
+
+    def _on_track(self, track: SoraMediaTrack):
+        if track.kind == "audio":
+            self._audio_stream_sink = SoraAudioStreamSink(
+                track, self._audio_output_frequency, self._audio_output_channels
+            )
+            self._audio_stream_sink.on_frame = self._on_frame
+
+    def disconnect(self):
+        self._connection.disconnect()
+
+
+def test_vad(setup):
+    signaling_urls = setup.get("signaling_urls")
+    channel_id_prefix = setup.get("channel_id_prefix")
+    metadata = setup.get("metadata")
+
+    channel_id = f"{channel_id_prefix}_{__name__}"
+
+    sendonly = VAD(signaling_urls, channel_id, metadata)
+    sendonly.connect()
+
+    time.sleep(5)
+
+    sendonly.disconnect()
