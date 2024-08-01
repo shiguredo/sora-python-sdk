cd9245b1758ceac6efa1d3bd7ffc51b5115e4040
voluntas <nakai@shiguredo.jp>
2024-08-01 16:21:05 +0900

    インスタンス化がまずいのか？

--- tests/test_ci.py
+++ tests/test_ci.py
@@ -1,8 +1,6 @@
-import json
 import sys
 import time
 import uuid
-from threading import Event
 
 from sora_sdk import Sora, SoraConnection, SoraVideoSource
 
@@ -13,20 +11,11 @@ class Sendonly:
         signaling_urls: list[str],
         channel_id: str,
         metadata: dict,
-        video_codec_type: str = "VP8",
     ):
         self._signaling_urls: list[str] = signaling_urls
         self._channel_id: str = channel_id
 
-        self._connection_id: str
-
-        # 接続した
-        self._connected: Event = Event()
-        # 終了
-        self._closed: Event = Event()
-
         self._sora: Sora = Sora()
-        self._connected = Event()
 
         self._video_source: SoraVideoSource = self._sora.create_video_source()
 
@@ -37,42 +26,11 @@ class Sendonly:
             metadata=metadata,
             audio=False,
             video=True,
-            video_codec_type=video_codec_type,
             video_source=self._video_source,
         )
 
-        self._connection.on_set_offer = self._on_set_offer
-        self._connection.on_notify = self._on_notify
-        self._connection.on_disconnect = self._on_disconnect
-
     def connect(self):
         self._connection.connect()
-
-        # _connected が set されるまで 30 秒待つ
-        assert self._connected.wait(30)
-
-        return self
-
-    def _on_set_offer(self, raw_offer):
-        offer = json.loads(raw_offer)
-        if offer["type"] == "offer":
-            self._connection_id = offer["connection_id"]
-            print(f"Offer を受信しました: connection_id={self._connection_id}")
-
-    def _on_notify(self, raw_message):
-        message = json.loads(raw_message)
-        if (
-            message["type"] == "notify"
-            and message["event_type"] == "connection.created"
-            and message["connection_id"] == self._connection_id
-        ):
-            print(f"Sora に接続しました: connection_id={self._connection_id}")
-            self._connected.set()
-
-    def _on_disconnect(self, error_code, message):
-        print(f"Sora から切断しました: error_code='{error_code}' message='{message}'")
-        self._closed.set()
-        self._connected.clear()
 
     def disconnect(self):
         self._connection.disconnect()
@@ -93,6 +51,6 @@ def test_sora(setup):
 
     sendonly.connect()
 
-    time.sleep(3)
+    time.sleep(5)
 
     sendonly.disconnect()
