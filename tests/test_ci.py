8ab05044a719beb7cbba03eae3618b53ca54bf23
voluntas <nakai@shiguredo.jp>
2024-08-01 15:00:49 +0900

    video_source

--- tests/test_ci.py
+++ tests/test_ci.py
@@ -5,6 +5,10 @@ import uuid
 from sora_sdk import Sora
 
 
+def _on_signaling_notify(message):
+    print(message)
+
+
 def test_sora(setup):
     signaling_urls = setup.get("signaling_urls")
     channel_id_prefix = setup.get("channel_id_prefix")
@@ -13,6 +17,9 @@ def test_sora(setup):
     channel_id = f"{channel_id_prefix}_{__name__}_{sys._getframe().f_code.co_name}_{uuid.uuid4()}"
 
     sora = Sora()
+
+    video_source = sora.create_video_source()
+
     connection = sora.create_connection(
         signaling_urls=signaling_urls,
         role="sendonly",
@@ -20,7 +27,10 @@ def test_sora(setup):
         metadata=metadata,
         audio=False,
         video=True,
+        video_source=video_source,
     )
+
+    # connection.on_notify = _on_signaling_notify
 
     connection.connect()
 
