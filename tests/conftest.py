02da857dba16be11f18c01a7a78b77718be38cf4
voluntas <nakai@shiguredo.jp>
2024-08-01 12:50:58 +0900

    signaling_urls を単体 or 複数でランダムでテストできるようにする

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import pytest
 from dotenv import load_dotenv
@@ -8,8 +9,15 @@ from dotenv import load_dotenv
 def setup():
     # 環境変数読み込み
     load_dotenv()
+
+    # signaling_url 単体か複数かをランダムで決めてテストする
+    signaling_url = os.environ.get("TEST_SIGNALING_URL")
+    if signaling_urls := os.environ.get("TEST_SIGNALING_URLS"):
+        signaling_urls = signaling_urls.split(",")
+    signaling_urls = random.choice([[signaling_url], signaling_urls])
+
     return {
-        "signaling_urls": [os.environ.get("TEST_SIGNALING_URL")],
+        "signaling_urls": signaling_urls,
         "channel_id_prefix": os.environ.get("TEST_CHANNEL_ID_PREFIX"),
         "secret": os.environ.get("TEST_SECRET_KEY"),
         "metadata": {"access_token": os.environ.get("TEST_SECRET_KEY")},
