4684d1301a258b77e930dbfed33fb0b1d10e01d6
voluntas <nakai@shiguredo.jp>
2024-08-13 15:01:48 +0900

    切り出し

--- examples/src/media/sendonly.py
+++ examples/src/media/sendonly.py
@@ -1,8 +1,57 @@
 import json
 import os
+import platform
 
+import cv2
 from dotenv import load_dotenv
 from media import Sendonly
+
+
+def get_video_capture(
+    camera_id: int,
+    video_width: int,
+    video_height: int,
+    video_fps: int,
+    video_fourcc: str,
+) -> cv2.VideoCapture:
+    """
+    ビデオキャプチャの設定を行います。
+
+    :param camera_id: 使用するカメラの ID
+    :param video_width: ビデオの幅
+    :param video_height: ビデオの高さ
+    :param video_fps: ビデオのフレームレート
+    :param video_fourcc: ビデオの FOURCC コード
+    """
+
+    if platform.system() == "Windows":
+        # CAP_DSHOW を設定しないと、カメラの起動がめちゃめちゃ遅くなる
+        video_capture = cv2.VideoCapture(camera_id, cv2.CAP_DSHOW)
+    else:
+        video_capture = cv2.VideoCapture(camera_id)
+
+    if video_width is not None:
+        video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, video_width)
+    if video_height is not None:
+        video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, video_height)
+    if video_fourcc is not None:
+        video_capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*video_fourcc))
+    if video_fps is not None:
+        video_capture.set(cv2.CAP_PROP_FPS, video_fps)
+
+    # Ubuntu → FOURCC を設定すると FPS が初期化される
+    # Windows → FPS を設定すると FOURCC が初期化される
+    # ので、両方に対応するため２回設定する
+    if video_fourcc is not None:
+        fourcc = cv2.VideoWriter_fourcc(*video_fourcc)
+        target_fourcc = video_capture.get(cv2.CAP_PROP_FOURCC)
+        if fourcc != target_fourcc:
+            video_capture.set(cv2.CAP_PROP_FOURCC, fourcc)
+    if video_fps is not None:
+        if video_fps != int(video_capture.get(cv2.CAP_PROP_FPS)):
+            video_capture.set(cv2.CAP_PROP_FPS, video_fps)
+
+    return video_capture
 
 
 def sendonly() -> None:
@@ -33,6 +82,14 @@ def sendonly() -> None:
 
     camera_id = int(os.getenv("SORA_CAMERA_ID", "0"))
 
+    video_capture = get_video_capture(
+        camera_id=camera_id,
+        video_width=video_width,
+        video_height=video_height,
+        video_fps=video_fps,
+        video_fourcc=video_fourcc,
+    )
+
     openh264_path = os.getenv("OPENH264_PATH")
 
     use_hwa = bool(os.getenv("USE_HWA", "True"))
@@ -43,13 +100,9 @@ def sendonly() -> None:
         metadata=metadata,
         video_codec_type=video_codec_type,
         video_bit_rate=video_bit_rate,
-        video_width=video_width,
-        video_height=video_height,
-        video_fps=video_fps,
-        video_fourcc=video_fourcc,
         openh264_path=openh264_path,
-        camera_id=camera_id,
         use_hwa=use_hwa,
+        video_capture=video_capture,
     )
     sendonly.run()
 
