e2ad7a606ba21f713b2fad4275c5214abd91dd22
voluntas <nakai@shiguredo.jp>
2024-09-16 21:55:44 +0900

    テスト圧縮

--- tests/test_sendonly_recvonly.py
+++ tests/test_sendonly_recvonly.py
@@ -2,6 +2,7 @@ import sys
 import time
 import uuid
 
+import pytest
 from client import Recvonly, Sendonly
 
 
@@ -57,7 +58,20 @@ def test_sendonly_recvonly_opus(setup):
     assert inbound_rtp_stats["packetsReceived"] > 0
 
 
-def test_sendonly_recvonly_vp8(setup):
+@pytest.fixture(
+    params=[
+        ("VP8", "libvpx", "libvpx"),
+        ("VP9", "libvpx", "libvpx"),
+        ("AV1", "libaom", "dav1d"),
+    ]
+)
+def video_codec_and_implementation(request):
+    return request.param
+
+
+def test_sendonly_recvonly(setup, video_codec_and_implementation):
+    video_codec, encoder_implementation, decoder_implementation = video_codec_and_implementation
+
     signaling_urls = setup.get("signaling_urls")
     channel_id_prefix = setup.get("channel_id_prefix")
     metadata = setup.get("metadata")
@@ -69,7 +83,7 @@ def test_sendonly_recvonly_vp8(setup):
         channel_id,
         audio=False,
         video=True,
-        video_codec_type="VP8",
+        video_codec_type=video_codec,
         metadata=metadata,
     )
     sendonly.connect(fake_video=True)
@@ -91,126 +105,20 @@ def test_sendonly_recvonly_vp8(setup):
 
     # codec が無かったら StopIteration 例外が上がる
     sendonly_codec_stats = next(s for s in sendonly_stats if s.get("type") == "codec")
-    assert sendonly_codec_stats["mimeType"] == "video/VP8"
+    assert sendonly_codec_stats["mimeType"] == f"video/{video_codec}"
 
     # outbound-rtp が無かったら StopIteration 例外が上がる
     outbound_rtp_stats = next(s for s in sendonly_stats if s.get("type") == "outbound-rtp")
-    assert outbound_rtp_stats["encoderImplementation"] == "libvpx"
+    assert outbound_rtp_stats["encoderImplementation"] == encoder_implementation
     assert outbound_rtp_stats["bytesSent"] > 0
     assert outbound_rtp_stats["packetsSent"] > 0
 
     # codec が無かったら StopIteration 例外が上がる
     recvonly_codec_stats = next(s for s in recvonly_stats if s.get("type") == "codec")
-    assert recvonly_codec_stats["mimeType"] == "video/VP8"
+    assert recvonly_codec_stats["mimeType"] == f"video/{video_codec}"
 
-    # outbound-rtp が無かったら StopIteration 例外が上がる
+    # inbound-rtp が無かったら StopIteration 例外が上がる
     inbound_rtp_stats = next(s for s in recvonly_stats if s.get("type") == "inbound-rtp")
-    assert inbound_rtp_stats["decoderImplementation"] == "libvpx"
-    assert inbound_rtp_stats["bytesReceived"] > 0
-    assert inbound_rtp_stats["packetsReceived"] > 0
-
-
-def test_sendonly_recvonly_vp9(setup):
-    signaling_urls = setup.get("signaling_urls")
-    channel_id_prefix = setup.get("channel_id_prefix")
-    metadata = setup.get("metadata")
-
-    channel_id = f"{channel_id_prefix}_{__name__}_{sys._getframe().f_code.co_name}_{uuid.uuid4()}"
-
-    sendonly = Sendonly(
-        signaling_urls,
-        channel_id,
-        audio=False,
-        video=True,
-        video_codec_type="VP9",
-        metadata=metadata,
-    )
-    sendonly.connect(fake_video=True)
-
-    recvonly = Recvonly(
-        signaling_urls,
-        channel_id,
-        metadata,
-    )
-    recvonly.connect()
-
-    time.sleep(5)
-
-    sendonly_stats = sendonly.get_stats()
-    recvonly_stats = recvonly.get_stats()
-
-    sendonly.disconnect()
-    recvonly.disconnect()
-
-    # codec が無かったら StopIteration 例外が上がる
-    sendonly_codec_stats = next(s for s in sendonly_stats if s.get("type") == "codec")
-    assert sendonly_codec_stats["mimeType"] == "video/VP9"
-
-    # outbound-rtp が無かったら StopIteration 例外が上がる
-    outbound_rtp_stats = next(s for s in sendonly_stats if s.get("type") == "outbound-rtp")
-    assert outbound_rtp_stats["encoderImplementation"] == "libvpx"
-    assert outbound_rtp_stats["bytesSent"] > 0
-    assert outbound_rtp_stats["packetsSent"] > 0
-
-    # codec が無かったら StopIteration 例外が上がる
-    recvonly_codec_stats = next(s for s in recvonly_stats if s.get("type") == "codec")
-    assert recvonly_codec_stats["mimeType"] == "video/VP9"
-
-    # outbound-rtp が無かったら StopIteration 例外が上がる
-    inbound_rtp_stats = next(s for s in recvonly_stats if s.get("type") == "inbound-rtp")
-    assert inbound_rtp_stats["decoderImplementation"] == "libvpx"
-    assert inbound_rtp_stats["bytesReceived"] > 0
-    assert inbound_rtp_stats["packetsReceived"] > 0
-
-
-def test_sendonly_recvonly_av1(setup):
-    signaling_urls = setup.get("signaling_urls")
-    channel_id_prefix = setup.get("channel_id_prefix")
-    metadata = setup.get("metadata")
-
-    channel_id = f"{channel_id_prefix}_{__name__}_{sys._getframe().f_code.co_name}_{uuid.uuid4()}"
-
-    sendonly = Sendonly(
-        signaling_urls,
-        channel_id,
-        audio=False,
-        video=True,
-        video_codec_type="AV1",
-        metadata=metadata,
-    )
-    sendonly.connect(fake_video=True)
-
-    recvonly = Recvonly(
-        signaling_urls,
-        channel_id,
-        metadata=metadata,
-    )
-    recvonly.connect()
-
-    time.sleep(5)
-
-    sendonly_stats = sendonly.get_stats()
-    recvonly_stats = recvonly.get_stats()
-
-    sendonly.disconnect()
-    recvonly.disconnect()
-
-    # codec が無かったら StopIteration 例外が上がる
-    sendonly_codec_stats = next(s for s in sendonly_stats if s.get("type") == "codec")
-    assert sendonly_codec_stats["mimeType"] == "video/AV1"
-
-    # outbound-rtp が無かったら StopIteration 例外が上がる
-    outbound_rtp_stats = next(s for s in sendonly_stats if s.get("type") == "outbound-rtp")
-    assert outbound_rtp_stats["encoderImplementation"] == "libaom"
-    assert outbound_rtp_stats["bytesSent"] > 0
-    assert outbound_rtp_stats["packetsSent"] > 0
-
-    # codec が無かったら StopIteration 例外が上がる
-    recvonly_codec_stats = next(s for s in recvonly_stats if s.get("type") == "codec")
-    assert recvonly_codec_stats["mimeType"] == "video/AV1"
-
-    # outbound-rtp が無かったら StopIteration 例外が上がる
-    inbound_rtp_stats = next(s for s in recvonly_stats if s.get("type") == "inbound-rtp")
-    assert inbound_rtp_stats["decoderImplementation"] == "dav1d"
+    assert inbound_rtp_stats["decoderImplementation"] == decoder_implementation
     assert inbound_rtp_stats["bytesReceived"] > 0
     assert inbound_rtp_stats["packetsReceived"] > 0
