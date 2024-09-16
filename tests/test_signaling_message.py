58ef9af85fbb4083d298f98a1ce35102ab24843c
voluntas <nakai@shiguredo.jp>
2024-09-16 16:37:36 +0900

    シグナリングメッセージのテストをランダムにする

--- tests/test_signaling_message.py
+++ tests/test_signaling_message.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import time
 import uuid
@@ -5,21 +6,24 @@ import uuid
 from client import Sendonly
 
 
-def test_signaling_message(setup):
+def test_random_signaling_message(setup):
     signaling_urls = setup.get("signaling_urls")
     channel_id_prefix = setup.get("channel_id_prefix")
     metadata = setup.get("metadata")
 
     channel_id = f"{channel_id_prefix}_{__name__}_{sys._getframe().f_code.co_name}_{uuid.uuid4()}"
 
+    selected_audio = random.choice([True, False])
+    selected_video = random.choice([True, False])
+
     sendonly = Sendonly(
         signaling_urls,
         channel_id,
-        audio=True,
-        video=True,
+        audio=selected_audio,
+        video=selected_video,
         metadata=metadata,
     )
-    sendonly.connect(fake_video=True, fake_audio=True)
+    sendonly.connect(fake_audio=selected_audio, fake_video=selected_video)
 
     time.sleep(5)
 
@@ -27,8 +31,8 @@ def test_signaling_message(setup):
     assert sendonly.offer_message is not None
     assert sendonly.answer_message is not None
 
-    assert sendonly.connect_message["audio"] is True
-    assert sendonly.connect_message["video"] is True
+    assert sendonly.connect_message["audio"] is selected_audio
+    assert sendonly.connect_message["video"] is selected_video
     assert sendonly.connect_message["metadata"] == metadata
 
     sendonly.disconnect()
