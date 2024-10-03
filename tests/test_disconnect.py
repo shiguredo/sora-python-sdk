6f9b3b9c1f9721cab6a3b55f2be76031fb417c99
voluntas <nakai@shiguredo.jp>
2024-10-03 21:50:15 +0900

    [e2e] api_url を使う準備

--- tests/test_disconnect.py
+++ tests/test_disconnect.py
@@ -4,6 +4,26 @@ import uuid
 from client import SoraClient, SoraRole
 
 
+def test_disconnect(setup):
+    signaling_urls = setup.get("signaling_urls")
+    channel_id_prefix = setup.get("channel_id_prefix")
+    metadata = setup.get("metadata")
+    api_url = setup.get("api_url")
+
+    channel_id = f"{channel_id_prefix}_{__name__}_{sys._getframe().f_code.co_name}_{uuid.uuid4()}"
+
+    with SoraClient(
+        signaling_urls,
+        SoraRole.RECVONLY,
+        channel_id,
+        audio=True,
+        video=True,
+        metadata=metadata,
+        data_channel_signaling=True,
+    ):
+        pass
+
+
 def test_sendonly_disconnect(setup):
     signaling_urls = setup.get("signaling_urls")
     channel_id_prefix = setup.get("channel_id_prefix")
