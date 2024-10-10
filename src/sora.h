4ec7deba415b67f6c9cbba24116d596be0387275
tnoho <teaotya@gmail.com>
2024-10-10 21:19:27 +0900

    引数の揺れを修正

--- src/sora.h
+++ src/sora.h
@@ -48,8 +48,8 @@ class Sora : public DisposePublisher {
    * @param signaling_notify_metadata (オプション)シグナリング通知メタデータ
    * @param audio_source (オプション)音声ソース CreateAudioSource で生成した SoraAudioSource を渡してください
    * @param video_source (オプション)映像ソース CreateVideoSource で生成した SoraVideoSource を渡してください
-   * @param audio_transformer (オプション)音声送信時の Encoded Transform
-   * @param video_transformer (オプション)映像送信時の Encoded Transform
+   * @param audio_frame_transformer (オプション)音声送信時の Encoded Transform
+   * @param video_frame_transformer (オプション)映像送信時の Encoded Transform
    * @param audio (オプション)音声の有効無効 デフォルト: true
    * @param video (オプション)映像の有効無効 デフォルト: true
    * @param audio_codec_type (オプション)音声コーデック OPUS デフォルト: OPUS
@@ -98,8 +98,8 @@ class Sora : public DisposePublisher {
       const nb::handle& signaling_notify_metadata,
       SoraTrackInterface* audio_source,
       SoraTrackInterface* video_source,
-      SoraAudioFrameTransformer* audio_transformer,
-      SoraVideoFrameTransformer* video_transformer,
+      SoraAudioFrameTransformer* audio_frame_transformer,
+      SoraVideoFrameTransformer* video_frame_transformer,
       std::optional<bool> audio,
       std::optional<bool> video,
       std::optional<std::string> audio_codec_type,
