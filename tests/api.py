800f4bfa918b80f6a6904a4b981ec1d1248d972c
voluntas <nakai@shiguredo.jp>
2024-10-22 10:16:40 +0900

    list_channel_connections_api を追加

--- tests/api.py
+++ tests/api.py
@@ -1,12 +1,22 @@
 import httpx
 
 
+def list_channel_connections_api(url: str, channel_id: str) -> httpx.Response:
+    headers = {
+        "Content-Type": "application/json",
+        "x-sora-target": "Sora_20201013.ListChannelConnections",
+    }
+    body = {"channel_id": channel_id}
+    return httpx.post(url, headers=headers, json=body, follow_redirects=True)
+
+
 def disconnect_channel_api(url: str, channel_id: str) -> httpx.Response:
     headers = {
         "Content-Type": "application/json",
         "x-sora-target": "Sora_20151104.DisconnectChannel",
     }
-    return httpx.post(url, headers=headers, json={"channel_id": channel_id}, follow_redirects=True)
+    body = {"channel_id": channel_id}
+    return httpx.post(url, headers=headers, json=body, follow_redirects=True)
 
 
 def disconnect_connection_api(url: str, channel_id: str, connection_id: str) -> httpx.Response:
