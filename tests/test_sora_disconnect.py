79be280063d00f420c8bcc8e66808b61c588724c
voluntas <nakai@shiguredo.jp>
2024-11-10 21:29:29 +0900

    lifetime expired を追加する

--- tests/test_sora_disconnect.py
+++ tests/test_sora_disconnect.py
@@ -2,11 +2,50 @@ import sys
 import time
 import uuid
 
+import jwt
 import pytest
 from api import disconnect_connection_api
 from client import SoraClient, SoraRole
 
 from sora_sdk import SoraSignalingErrorCode
+
+
+def test_websocket_signaling_only_lifetime_expired(setup):
+    signaling_urls = setup.get("signaling_urls")
+    channel_id_prefix = setup.get("channel_id_prefix")
+    secret = setup.get("secret")
+
+    channel_id = f"{channel_id_prefix}_{__name__}_{sys._getframe().f_code.co_name}_{uuid.uuid4()}"
+
+    access_token = jwt.encode(
+        {
+            "channel_id": channel_id,
+            "audio": False,
+            "video": True,
+            "connection_lifetime": 3,
+            # 現在時刻 + 300 秒 (5分)
+            "exp": int(time.time()) + 300,
+        },
+        secret,
+        algorithm="HS256",
+    )
+    metadata = {"access_token": access_token}
+
+    with SoraClient(
+        signaling_urls,
+        SoraRole.RECVONLY,
+        channel_id,
+        audio=True,
+        video=True,
+        metadata=metadata,
+        data_channel_signaling=False,
+        ignore_disconnect_websocket=False,
+    ) as conn:
+        time.sleep(5)
+
+        assert conn.ws_close is True
+        assert conn.ws_close_code == 1000
+        assert conn.ws_close_reason == "LIFETIME-EXPIRED"
 
 
 @pytest.mark.skipif(sys.platform != "linux", reason="linux でのみ実行する")
@@ -46,8 +85,6 @@ def test_websocket_signaling_only_disconnect_api(setup):
         # assert conn.disconnect_reason is not None
         # assert "DISCONNECTED-API" in conn.disconnect_reason
 
-    # TODO: LIFETIME-EXPIRED のテスト
-
 
 @pytest.mark.skipif(sys.platform != "linux", reason="linux でのみ実行する")
 def test_websocket_datachannel_signaling_disconnect_api(setup):
@@ -86,7 +123,43 @@ def test_websocket_datachannel_signaling_disconnect_api(setup):
         # assert conn.disconnect_reason is not None
         # assert "DISCONNECTED-API" in conn.disconnect_reason
 
-    # TODO: LIFETIME-EXPIRED のテスト
+
+def test_websocket_datachannel_signaling_lifetime_expired(setup):
+    signaling_urls = setup.get("signaling_urls")
+    channel_id_prefix = setup.get("channel_id_prefix")
+    secret = setup.get("secret")
+
+    channel_id = f"{channel_id_prefix}_{__name__}_{sys._getframe().f_code.co_name}_{uuid.uuid4()}"
+
+    access_token = jwt.encode(
+        {
+            "channel_id": channel_id,
+            "audio": True,
+            "video": True,
+            "connection_lifetime": 3,
+            # 現在時刻 + 300 秒 (5分)
+            "exp": int(time.time()) + 300,
+        },
+        secret,
+        algorithm="HS256",
+    )
+    metadata = {"access_token": access_token}
+
+    with SoraClient(
+        signaling_urls,
+        SoraRole.RECVONLY,
+        channel_id,
+        audio=True,
+        video=True,
+        metadata=metadata,
+        data_channel_signaling=True,
+        ignore_disconnect_websocket=False,
+    ) as conn:
+        time.sleep(5)
+
+        assert conn.ws_close is True
+        assert conn.ws_close_code == 1000
+        assert conn.ws_close_reason == "LIFETIME-EXPIRED"
 
 
 @pytest.mark.skipif(sys.platform != "linux", reason="linux でのみ実行する")
