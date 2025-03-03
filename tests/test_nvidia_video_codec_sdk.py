3bf6497ef49c68ad95d3afd83fbdaf7f24408e20
voluntas <nakai@shiguredo.jp>
2025-03-03 12:26:01 +0900

    コード整理

--- tests/test_nvidia_video_codec_sdk.py
+++ tests/test_nvidia_video_codec_sdk.py
@@ -17,7 +17,6 @@ from sora_sdk import (
 )
 
 
-# @pytest.mark.skip()
 @pytest.mark.skipif(
     os.environ.get("NVIDIA_VIDEO_SDK") is None, reason="NVIDIA Video Codec SDK でのみ実行する"
 )
@@ -32,7 +31,7 @@ from sora_sdk import (
         ("H265", "NvCodec"),
     ],
 )
-def test_nvidia_codec_sdk_sendonly(setup, video_codec_type, expected_implementation):
+def test_nvidia_codec_sdk_sendonly_recvonly(setup, video_codec_type, expected_implementation):
     if not is_codec_supported(
         video_codec_type, SoraVideoCodecImplementation.NVIDIA_VIDEO_CODEC_SDK
     ):
@@ -63,12 +62,29 @@ def test_nvidia_codec_sdk_sendonly(setup, video_codec_type, expected_implementat
     )
     sendonly.connect(fake_video=True)
 
+    recvonly = SoraClient(
+        signaling_urls,
+        SoraRole.RECVONLY,
+        channel_id,
+        metadata=metadata,
+        video_codec_preference=SoraVideoCodecPreference(
+            codecs=[
+                SoraVideoCodecPreference.Codec(
+                    type=codec_type_string_to_codec_type(video_codec_type),
+                    decoder=SoraVideoCodecImplementation.NVIDIA_VIDEO_CODEC_SDK,
+                ),
+            ]
+        ),
+    )
+    recvonly.connect()
+
     time.sleep(5)
 
-    assert sendonly.connect_message is not None
-    assert sendonly.connect_message["channel_id"] == channel_id
-    assert "video" in sendonly.connect_message
-    assert sendonly.connect_message["video"]["codec_type"] == video_codec_type
+    sendonly_stats = sendonly.get_stats()
+    recvonly_stats = recvonly.get_stats()
+
+    sendonly.disconnect()
+    recvonly.disconnect()
 
     # offer の sdp に video_codec_type が含まれているかどうかを確認している
     assert sendonly.offer_message is not None
@@ -80,20 +96,27 @@ def test_nvidia_codec_sdk_sendonly(setup, video_codec_type, expected_implementat
     assert "sdp" in sendonly.answer_message
     assert video_codec_type in sendonly.answer_message["sdp"]
 
-    sendonly_stats = sendonly.get_stats()
-
-    sendonly.disconnect()
-
     # codec が無かったら StopIteration 例外が上がる
-    codec_stats = next(s for s in sendonly_stats if s.get("type") == "codec")
-    # H.264 が採用されているかどうか確認する
-    assert codec_stats["mimeType"] == f"video/{video_codec_type}"
+    sendonly_codec_stats = next(s for s in sendonly_stats if s.get("type") == "codec")
+    # H.264/H.265 が採用されているかどうか確認する
+    assert sendonly_codec_stats["mimeType"] == f"video/{video_codec_type}"
 
     # outbound-rtp が無かったら StopIteration 例外が上がる
     outbound_rtp_stats = next(s for s in sendonly_stats if s.get("type") == "outbound-rtp")
     assert outbound_rtp_stats["encoderImplementation"] == expected_implementation
     assert outbound_rtp_stats["bytesSent"] > 0
     assert outbound_rtp_stats["packetsSent"] > 0
+
+    # codec が無かったら StopIteration 例外が上がる
+    recvonly_codec_stats = next(s for s in recvonly_stats if s.get("type") == "codec")
+    # H.264/H.265 が採用されているかどうか確認する
+    assert recvonly_codec_stats["mimeType"] == f"video/{video_codec_type}"
+
+    # inbound-rtp が無かったら StopIteration 例外が上がる
+    inbound_rtp_stats = next(s for s in recvonly_stats if s.get("type") == "inbound-rtp")
+    assert inbound_rtp_stats["decoderImplementation"] == expected_implementation
+    assert inbound_rtp_stats["bytesReceived"] > 0
+    assert inbound_rtp_stats["packetsReceived"] > 0
 
 
 @pytest.mark.skipif(
@@ -137,7 +160,7 @@ def test_nvidia_codec_sdk_sendonly(setup, video_codec_type, expected_implementat
         # ("H265", "NvCodec", 101, 240, 135, 1),
     ],
 )
-def test_nvidia_codec_sdk_simulcast(
+def test_nvidia_video_codec_sdk_simulcast(
     setup,
     video_codec_type,
     expected_implementation,
@@ -245,109 +268,6 @@ def test_nvidia_codec_sdk_simulcast(
             )
             assert s["bytesSent"] == 0
             assert s["packetsSent"] <= 2
-
-
-# @pytest.mark.skip()
-@pytest.mark.skipif(
-    os.environ.get("NVIDIA_VIDEO_SDK") is None, reason="NVIDIA Video Codec SDK でのみ実行する"
-)
-@pytest.mark.parametrize(
-    (
-        "video_codec_type",
-        "expected_implementation",
-    ),
-    [
-        ("AV1", "NvCodec"),
-        ("H264", "NvCodec"),
-        ("H265", "NvCodec"),
-    ],
-)
-def test_nvidia_codec_sdk_sendonly_recvonly(setup, video_codec_type, expected_implementation):
-    if not is_codec_supported(
-        video_codec_type, SoraVideoCodecImplementation.NVIDIA_VIDEO_CODEC_SDK
-    ):
-        pytest.skip(f"このチップでは {video_codec_type} がサポートされていません")
-
-    signaling_urls = setup.get("signaling_urls")
-    channel_id_prefix = setup.get("channel_id_prefix")
-    metadata = setup.get("metadata")
-
-    channel_id = f"{channel_id_prefix}_{__name__}_{sys._getframe().f_code.co_name}_{uuid.uuid4()}"
-
-    sendonly = SoraClient(
-        signaling_urls,
-        SoraRole.SENDONLY,
-        channel_id,
-        audio=False,
-        video=True,
-        video_codec_type=video_codec_type,
-        metadata=metadata,
-        video_codec_preference=SoraVideoCodecPreference(
-            codecs=[
-                SoraVideoCodecPreference.Codec(
-                    type=codec_type_string_to_codec_type(video_codec_type),
-                    encoder=SoraVideoCodecImplementation.NVIDIA_VIDEO_CODEC_SDK,
-                ),
-            ]
-        ),
-    )
-    sendonly.connect(fake_video=True)
-
-    recvonly = SoraClient(
-        signaling_urls,
-        SoraRole.RECVONLY,
-        channel_id,
-        metadata=metadata,
-        video_codec_preference=SoraVideoCodecPreference(
-            codecs=[
-                SoraVideoCodecPreference.Codec(
-                    type=codec_type_string_to_codec_type(video_codec_type),
-                    decoder=SoraVideoCodecImplementation.NVIDIA_VIDEO_CODEC_SDK,
-                ),
-            ]
-        ),
-    )
-    recvonly.connect()
-
-    time.sleep(5)
-
-    sendonly_stats = sendonly.get_stats()
-    recvonly_stats = recvonly.get_stats()
-
-    sendonly.disconnect()
-    recvonly.disconnect()
-
-    # offer の sdp に video_codec_type が含まれているかどうかを確認している
-    assert sendonly.offer_message is not None
-    assert "sdp" in sendonly.offer_message
-    assert video_codec_type in sendonly.offer_message["sdp"]
-
-    # answer の sdp に video_codec_type が含まれているかどうかを確認している
-    assert sendonly.answer_message is not None
-    assert "sdp" in sendonly.answer_message
-    assert video_codec_type in sendonly.answer_message["sdp"]
-
-    # codec が無かったら StopIteration 例外が上がる
-    sendonly_codec_stats = next(s for s in sendonly_stats if s.get("type") == "codec")
-    # H.264/H.265 が採用されているかどうか確認する
-    assert sendonly_codec_stats["mimeType"] == f"video/{video_codec_type}"
-
-    # outbound-rtp が無かったら StopIteration 例外が上がる
-    outbound_rtp_stats = next(s for s in sendonly_stats if s.get("type") == "outbound-rtp")
-    assert outbound_rtp_stats["encoderImplementation"] == expected_implementation
-    assert outbound_rtp_stats["bytesSent"] > 0
-    assert outbound_rtp_stats["packetsSent"] > 0
-
-    # codec が無かったら StopIteration 例外が上がる
-    recvonly_codec_stats = next(s for s in recvonly_stats if s.get("type") == "codec")
-    # H.264/H.265 が採用されているかどうか確認する
-    assert recvonly_codec_stats["mimeType"] == f"video/{video_codec_type}"
-
-    # inbound-rtp が無かったら StopIteration 例外が上がる
-    inbound_rtp_stats = next(s for s in recvonly_stats if s.get("type") == "inbound-rtp")
-    assert inbound_rtp_stats["decoderImplementation"] == expected_implementation
-    assert inbound_rtp_stats["bytesReceived"] > 0
-    assert inbound_rtp_stats["packetsReceived"] > 0
 
 
 # VP8 / VP9 は HWA Decoder のみ搭載している
