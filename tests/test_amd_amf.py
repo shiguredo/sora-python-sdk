1d87832fa217011c97840ef956801e0ade90f5bf
voluntas <nakai@shiguredo.jp>
2025-03-05 17:45:15 +0900

    simulcast を追加

--- tests/test_amd_amf.py
+++ tests/test_amd_amf.py
@@ -44,6 +44,7 @@ def test_amd_amf_available(setup):
                         assert c.decoder is True
                         assert c.encoder is False
                     case SoraVideoCodecType.AV1:
+                        # TODO: AV1 decoder は True だが色々課題あり
                         assert c.decoder is True
                         assert c.encoder is True
                     case SoraVideoCodecType.H264:
@@ -152,3 +153,158 @@ def test_amd_amf_sendonly_recvonly(setup, video_codec_type):
     assert inbound_rtp_stats["decoderImplementation"] == "AMF"
     assert inbound_rtp_stats["bytesReceived"] > 0
     assert inbound_rtp_stats["packetsReceived"] > 0
+
+
+@pytest.mark.skipif(os.environ.get("AMD_AMF") is None, reason="AMD AMF でのみ実行する")
+@pytest.mark.parametrize(
+    (
+        "video_codec_type",
+        "expected_implementation",
+        "video_bit_rate",
+        "video_width",
+        "video_height",
+        "simulcast_count",
+    ),
+    # FIXME: H.265 では、本数が 2 本 や 1 本の場合、エラーになるのでコメントアウトしている
+    # FIXME: AV1 では、解像度が一定数より低くなる場合、エラーになるのでコメントアウトしている
+    [
+        # 1080p
+        ("AV1", "AMF", 5000, 1920, 1080, 3),
+        ("H264", "AMF", 5000, 1920, 1080, 3),
+        ("H265", "AMF", 5000, 1920, 1080, 3),
+        # 720p
+        ("AV1", "AMF", 2500, 1280, 720, 3),
+        ("H264", "AMF", 2500, 1280, 720, 3),
+        ("H265", "AMF", 2500, 1280, 720, 3),
+        # 540p
+        ("AV1", "AMF", 1200, 960, 540, 3),
+        ("H264", "AMF", 1200, 960, 540, 3),
+        ("H265", "AMF", 1200, 960, 540, 3),
+        # 360p
+        # ("AV1", "AMF", 700, 640, 360, 2),
+        ("H264", "AMF", 700, 640, 360, 2),
+        # ("H265", "AMF", 700, 640, 360, 2),
+        # 270p
+        # ("AV1", "AMF", 450, 480, 270, 2),
+        ("H264", "AMF", 450, 480, 270, 2),
+        # ("H265", "AMF", 257, 480, 270, 2),
+        # 180p
+        # ("AV1", "AMF", 200, 320, 180, 1),
+        ("H264", "AMF", 200, 320, 180, 1),
+        # ("H265", "AMF", 142, 320, 180, 1),
+        # 135p
+        # ("H265", "AMF", 101, 240, 135, 1),
+    ],
+)
+def test_amd_amf_simulcast(
+    setup,
+    video_codec_type,
+    expected_implementation,
+    video_bit_rate,
+    video_width,
+    video_height,
+    simulcast_count,
+):
+    if not is_codec_supported(video_codec_type, SoraVideoCodecImplementation.AMD_AMF):
+        pytest.skip(f"このチップでは {video_codec_type} のエンコードがサポートされていません")
+
+    signaling_urls = setup.get("signaling_urls")
+    channel_id_prefix = setup.get("channel_id_prefix")
+    metadata = setup.get("metadata")
+
+    channel_id = f"{channel_id_prefix}_{__name__}_{sys._getframe().f_code.co_name}_{uuid.uuid4()}"
+
+    sendonly = SoraClient(
+        signaling_urls,
+        SoraRole.SENDONLY,
+        channel_id,
+        simulcast=True,
+        audio=False,
+        video=True,
+        video_codec_type=video_codec_type,
+        video_bit_rate=video_bit_rate,
+        metadata=metadata,
+        video_width=video_width,
+        video_height=video_height,
+        video_codec_preference=SoraVideoCodecPreference(
+            codecs=[
+                SoraVideoCodecPreference.Codec(
+                    type=codec_type_string_to_codec_type(video_codec_type),
+                    encoder=SoraVideoCodecImplementation.AMD_AMF,
+                ),
+            ]
+        ),
+    )
+    sendonly.connect(fake_video=True)
+
+    time.sleep(5)
+
+    sendonly_stats = sendonly.get_stats()
+
+    sendonly.disconnect()
+
+    # "type": "answer" の SDP で Simulcast があるかどうか
+    assert sendonly.answer_message is not None
+    assert "sdp" in sendonly.answer_message
+    assert "a=simulcast:send r0;r1;r2" in sendonly.answer_message["sdp"]
+
+    # codec が無かったら StopIteration 例外が上がる
+    sendonly_codec_stats = next(s for s in sendonly_stats if s.get("type") == "codec")
+    assert sendonly_codec_stats["mimeType"] == f"video/{video_codec_type}"
+
+    # 複数の outbound-rtp 統計情報を取得
+    outbound_rtp_stats = [
+        s for s in sendonly_stats if s.get("type") == "outbound-rtp" and s.get("kind") == "video"
+    ]
+    # simulcast_count に関係なく統計情報はかならず 3 本出力される
+    # これは SDP で rid で ~r0 とかやる減るはず
+    assert len(outbound_rtp_stats) == 3
+
+    # rid でソート
+    sorted_stats = sorted(outbound_rtp_stats, key=lambda x: x.get("rid", ""))
+
+    for i, s in enumerate(sorted_stats):
+        assert s["rid"] == f"r{i}"
+        # simulcast_count が 2 の場合、rid r2 の bytesSent/packetsSent は 0 or 1 になる
+        # simulcast_count が 1 の場合、rid r2 と r1 の bytesSent/packetsSent は 0 or 1 になる
+        if i < simulcast_count:
+            # 1 本になると simulcastEncodingAdapter がなくなる
+            # if simulcast_count > 1:
+            #     assert "SimulcastEncoderAdapter" in s["encoderImplementation"]
+
+            # targetBitrate が指定したビットレートの 90% 以上、100% 以下に収まることを確認
+            expected_bitrate = video_bit_rate * 1000
+
+            # パケットが一切送られてこない場合は frame_width/frame_height が含まれないので None になる
+            frame_width = s.get("frameWidth")
+            frame_height = s.get("frameHeight")
+            encoder_implementation = s.get("encoderImplementation")
+
+            print(
+                s["rid"],
+                video_codec_type,
+                expected_bitrate,
+                s["targetBitrate"],
+                expected_implementation,
+                encoder_implementation,
+                frame_width,
+                frame_height,
+                s["bytesSent"],
+                s["packetsSent"],
+            )
+            assert s["bytesSent"] > 1000
+            assert s["packetsSent"] > 5
+            # 期待値の 20% 以上、100% 以下に収まることを確認
+            assert expected_bitrate * 0.2 <= s["targetBitrate"] <= expected_bitrate
+        else:
+            # 本来は 0 なのだが、simulcast_count が 1 の場合、
+            # packetSent が 0 ではなく 1 や 2 になる場合がある
+            # byteSent は 0
+            print(
+                s["rid"],
+                video_codec_type,
+                s["bytesSent"],
+                s["packetsSent"],
+            )
+            assert s["bytesSent"] == 0
+            assert s["packetsSent"] <= 2
