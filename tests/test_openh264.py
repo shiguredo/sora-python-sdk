15c8011f960d6c22becca8eb2c7668267af981b5
voluntas <nakai@shiguredo.jp>
2025-04-22 19:35:28 +0900

    フレーキーテストと戦う

--- tests/test_openh264.py
+++ tests/test_openh264.py
@@ -225,7 +225,7 @@ def test_openh264_simulcast(
     )
     sendonly.connect(fake_video=True)
 
-    time.sleep(5)
+    time.sleep(10)
 
     sendonly_stats = sendonly.get_stats()
 
@@ -251,12 +251,7 @@ def test_openh264_simulcast(
         assert "qualityLimitationDurations" in s
 
         # qualityLimitationReason が none で無い場合は安定したテストができない
-        # さらに frameWidth/frameHeight がない場合は送られてきてすらいないのでテストをスキップしてしまう
-        if (
-            s["qualityLimitationReason"] != "none"
-            and "frameWidth" not in s
-            and "frameHeight" not in s
-        ):
+        if s["qualityLimitationReason"] != "none":
             pytest.skip(f"qualityLimitationReason: {s['qualityLimitationReason']}")
 
         assert s["rid"] == f"r{i}"
