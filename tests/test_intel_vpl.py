53490796098815744600593d85a4c9598e43ad5b
voluntas <nakai@shiguredo.jp>
2025-08-06 22:17:35 +0900

    Intel VPL AV1 の Descriptor RTP ヘッダー拡張に関する xfail マークを削除する

--- tests/test_intel_vpl.py
+++ tests/test_intel_vpl.py
@@ -554,10 +554,6 @@ def test_intel_vpl_decode(
     assert inbound_rtp_stats["keyFramesDecoded"] > 0
 
 
-@pytest.mark.xfail(
-    strict=True,
-    reason="Intel VPL AV1 の Descriptor RTP ヘッダー拡張が answer SDP に含まれないため",
-)
 @pytest.mark.skipif(os.environ.get("INTEL_VPL") is None, reason="Intel VPL でのみ実行する")
 def test_intel_vpl_av1_rtp_hdr_ext(settings):
     sendonly = SoraClient(
